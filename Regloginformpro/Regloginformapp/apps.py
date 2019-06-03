5194611d0a6a44a369870fe76678a45c998193d5
MonuRanjan <thakurranjan16@gmail.com>
2019-06-03 12:31:00 +0530

    first

---
+++ Regloginformpro/Regloginformapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegloginformappConfig(AppConfig):
+    name = 'Regloginformapp'
---
+++ Regloginformpro/Regloginformapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class RegistrationData(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    number = models.BigIntegerField()
+
+    def __str__(self):
+        self.first_name
